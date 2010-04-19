9c8a6abf1c772f910b090a438f78e30960391dd5
Jesse Lovelace <jesse@keyingredient.com>
2010-04-19 17:03:13 -0500

    Add a new property that doesn't deref.

--- properties.py
+++ properties.py
@@ -1,7 +1,15 @@
 from google.appengine.ext import db
 from models import Image
 
-class ImageProperty(db.ReferenceProperty):
+class ImageProperty(db.Property):
 	
-	def __init__(self, **attrs):
-		super(ImageProperty, self).__init__(Image, **attrs)
+	def validate(self, value):
+		if isinstance(value, basestring):
+			value = db.Key(value)
+		
+		if value is not None:
+			if not isinstance(value, db.Key):
+				raise TypeError("Property %s must be an instance of db.Key"
+							% (self.name,))
+		return super(ImageProperty, self).validate(value)
+		
